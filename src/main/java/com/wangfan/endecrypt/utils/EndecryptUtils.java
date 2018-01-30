2352748eb0c52c8e194572e68be9e745f4ff7f8b
synchronized <whvcse@foxmail.com>
2018-01-30 09:56:51 +0800

    加入异常捕获，出现异常返回null

--- src/main/java/com/wangfan/endecrypt/utils/EndecryptUtils.java
+++ src/main/java/com/wangfan/endecrypt/utils/EndecryptUtils.java
@@ -8,7 +8,7 @@ import com.wangfan.endecrypt.crypto.AesCipherService;
 import com.wangfan.endecrypt.crypto.hash.Md5Hash;
 
 /**
- * 加密解密工具类 
+ * 加密解密工具类
  * 
  * @author wangfan
  * @date 2017-4-28 上午9:45:00
@@ -21,7 +21,12 @@ public class EndecryptUtils {
 	 * @return
 	 */
 	public static String encrytBase64(String str) {
-		return Base64.encodeToString(str.getBytes());
+		try {
+			return Base64.encodeToString(str.getBytes());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 	/**
@@ -31,7 +36,12 @@ public class EndecryptUtils {
 	 * @return
 	 */
 	public static String decryptBase64(String cipherText) {
-		return Base64.decodeToString(cipherText);
+		try {
+			return Base64.decodeToString(cipherText);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 	/**
@@ -41,7 +51,12 @@ public class EndecryptUtils {
 	 * @return
 	 */
 	public static String encrytHex(String str) {
-		return Hex.encodeToString(str.getBytes());
+		try {
+			return Hex.encodeToString(str.getBytes());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 	/**
@@ -51,33 +66,54 @@ public class EndecryptUtils {
 	 * @return
 	 */
 	public static String decryptHex(String cipherText) {
-		return new String(Hex.decode(cipherText));
+		try {
+			return new String(Hex.decode(cipherText));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
-	
+
 	/**
 	 * aes加密
 	 * 
-	 * @param password 明文
-	 * @param key 密钥
+	 * @param password
+	 *            明文
+	 * @param key
+	 *            密钥
 	 * @return
 	 */
-	public static String encrytAes(String password, Key key){
-		AesCipherService aesCipherService = new AesCipherService();
-		String aes_cipherText = aesCipherService.encrypt(password.getBytes(), key.getEncoded()).toHex();
-		return aes_cipherText;
+	public static String encrytAes(String password, Key key) {
+		try {
+			AesCipherService aesCipherService = new AesCipherService();
+			String aes_cipherText = aesCipherService.encrypt(
+					password.getBytes(), key.getEncoded()).toHex();
+			return aes_cipherText;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
-	
+
 	/**
 	 * aes解密
 	 * 
-	 * @param cipherText 密文
-	 * @param key 密钥
+	 * @param cipherText
+	 *            密文
+	 * @param key
+	 *            密钥
 	 * @return
 	 */
-	public static String decryptAes(String cipherText, Key key){
-		AesCipherService aesCipherService = new AesCipherService();
-		String aes_mingwen = new String(aesCipherService.decrypt(Hex.decode(cipherText), key.getEncoded()).getBytes());
-		return aes_mingwen;
+	public static String decryptAes(String cipherText, Key key) {
+		try {
+			AesCipherService aesCipherService = new AesCipherService();
+			String aes_mingwen = new String(aesCipherService.decrypt(
+					Hex.decode(cipherText), key.getEncoded()).getBytes());
+			return aes_mingwen;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 	/**
@@ -87,30 +123,51 @@ public class EndecryptUtils {
 	 * @author wangfan
 	 */
 	public static Key generateKey(String key) {
-		AesCipherService aesCipherService = new AesCipherService();
-		return aesCipherService.generateKey(key);
+		try {
+			AesCipherService aesCipherService = new AesCipherService();
+			return aesCipherService.generateKey(key);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 	/**
 	 * md5加密(没有盐)
 	 * 
-	 * @param password 要加密的字符串
+	 * @param password
+	 *            要加密的字符串
 	 */
 	public static String encrytMd5(String password) {
-		return new Md5Hash(password).toHex();
+		try {
+			return new Md5Hash(password).toHex();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
-	
+
 	/**
 	 * 指定盐salt进行md5加密
 	 * 
-	 * @param password 要加密的字符串
-	 * @param salt 盐
-	 * @param hashIterations 散列次数
+	 * @param password
+	 *            要加密的字符串
+	 * @param salt
+	 *            盐
+	 * @param hashIterations
+	 *            散列次数
 	 * @return
 	 */
-	public static String encrytMd5(String password, String salt, int hashIterations) {
-		String password_cipherText = new Md5Hash(password, salt, hashIterations).toHex();
-		return password_cipherText;
+	public static String encrytMd5(String password, String salt,
+			int hashIterations) {
+		try {
+			String password_cipherText = new Md5Hash(password, salt,
+					hashIterations).toHex();
+			return password_cipherText;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 }
